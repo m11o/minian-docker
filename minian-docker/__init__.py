0c6ed79af5eecdb019e59114eb415be9509e2df6
masahito1997 <velonica.gale2227@gmail.com>
2021-10-26 10:50:13 +0900

    rename minian_docker directory

--- minian-docker/__init__.py
+++
@@ -1,4 +0,0 @@
-from ..minian_docker import *
-from motd import *
-from version import *
-from docker import *
---
+++ minian_docker/__init__.py
@@ -0,0 +1,3 @@
+from minian_docker.motd import *
+from minian_docker.version import *
+from minian_docker.docker import *
