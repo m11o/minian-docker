955d6544910e48930fff564255fe2b8b9973ed09
masahito1997 <velonica.gale2227@gmail.com>
2021-11-02 12:08:46 +0900

    add gui in command option

--- minian_docker/bin.py
+++ minian_docker/bin.py
@@ -12,7 +12,7 @@ def _parse_args():
     parser.add_argument(
         'container',
         type=str,
-        choices=['notebook', 'bash'],
+        choices=['notebook', 'bash', 'gui'],
         help='select a docker container type to launch for minian'
     )
     return parser.parse_known_args()
